89eaa5c7e77a211787338a9d02399a06fb138c87
Adnan Gazi <adnan.gazi01@gmail.com>
2023-12-08 16:43:19 +0000

    day 1 pt 2 impl

--- src/day1.rs
+++ src/day1.rs
@@ -3,13 +3,12 @@ use std::str::{FromStr, Lines};
 use regex::{Captures, Regex};
 
 pub fn calc(input: &str) -> Option<usize> {
-
-    let lines = input.lines();
+    let lines= input.lines().map(String::from).collect();
 
     map_lines(lines)
 }
 
-fn map_lines(lines: Lines) -> Option<usize> {
+fn map_lines(lines: Vec<String>) -> Option<usize> {
     fn extract(cap: Captures) -> Option<usize> {
         cap.get(1)
             .map(|t| { usize::from_str(t.as_str()) })?
@@ -21,9 +20,9 @@ fn map_lines(lines: Lines) -> Option<usize> {
     let mut nums = vec![];
     for line in lines {
         print!("{}", line);
-        let m = r1.captures(line);
+        let m = r1.captures(line.as_str());
         //print!(" {:?}", m);
-        let n = r2.captures(line);
+        let n = r2.captures(line.as_str());
         //print!(" {:?}", n);
 
         let left = m.and_then(extract);
@@ -41,54 +40,25 @@ fn map_lines(lines: Lines) -> Option<usize> {
 }
 
 pub fn calc_2(input: &str) -> Option<usize> {
-    let matchers = HashMap::from(
-        [("one", "1"),
-            ("two", "2"),
-            ("three", "3"),
-            ("four", "4"),
-            ("five", "5"),
-            ("six", "6"),
-            ("seven", "7"),
-            ("eight", "8"),
-            ("nine", "9")]
-    );
+    let matchers = HashMap::from([
+        ("one", "one1one"),
+        ("two", "two2two"),
+        ("three", "three3three"),
+        ("four", "four4four"),
+        ("five", "five5five"),
+        ("six", "six6six"),
+        ("seven", "seven7seven"),
+        ("eight", "eight8eight"),
+        ("nine", "nine9nine")
+    ]);
 
-    let mut nums = vec![];
-
-    for line in input.lines() {
-
-        let mut matches = HashMap::new();
-        for (m, i) in matchers.iter() {
-            line
-                .find(*m)
-                .and_then(|u| {matches.insert(u, *m)});
-            line.find(*i)
-                .and_then(|u| {matches.insert(u, *i)});
+    let new_in = input.lines().map(|l| {
+        let mut r = String::from(l);
+        for (k, v) in matchers.iter() {
+            r = r.replace(k, v);
         }
+        r
+    }).collect();
 
-        print!("{line}");
-
-        let get_string_value_at_index = |f: &usize| {
-            matches.get(f).and_then(|x| matchers.get(*x).or(Some(x)))
-        };
-
-        let min = matches.keys().min();
-        let first_num = min
-            .and_then(get_string_value_at_index)
-            .and_then(|v| {usize::from_str(v).ok()})
-            .unwrap_or(0);
-
-        let max = matches.keys().max();
-        let last_num = max
-            .and_then(get_string_value_at_index)
-            .and_then(|v| {usize::from_str(v).ok()})
-            .unwrap_or(0);
-
-        println!(" {:?} {first_num}, {:?} {last_num}", min, max);
-        println!("{:?}", matches);
-
-        nums.push((10 * first_num) + last_num)
-    }
-
-    Some(nums.iter().sum())
+    return map_lines(new_in);
 }
